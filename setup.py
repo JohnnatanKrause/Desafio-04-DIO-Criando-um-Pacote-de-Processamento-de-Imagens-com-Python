12ee34aca4a7231556ea0bbd5f4bf5bf892fa599
Johnnatan Krause <johnnatankrause@gmail.com>
2024-10-25 15:50:14 -0300

    Alterando o nome do pacote de package_neme para meu pacote unico, estava dando conflito na hora de realizar o upload

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open("requirements.txt") as f:
     requirements = f.read().splitlines()
 
 setup(
-    name="package_name",
+    name="meu_pacote_unico",
     version="0.0.1",
     author="Johnnatan_Krause",
     author_email="johnnatankrause@gmail.com",
